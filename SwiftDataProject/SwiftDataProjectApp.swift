b34b051e6927ef21e08eeff808831c9cecdda9a7
Álvaro Gascón <alvarogascon@hotmail.com>
2024-06-10 21:54:11 +0200

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Álvaro Gascón on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
