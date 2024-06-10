f11554461e4c87d395a233aba713f14ae0ad2f3d
Álvaro Gascón <alvarogascon@hotmail.com>
2024-06-10 22:16:46 +0200

    End of PartOne

---
+++ SwiftDataProject/EditUserView.swift
@@ -0,0 +1,18 @@
+//
+//  EditUserView.swift
+//  SwiftDataProject
+//
+//  Created by Álvaro Gascón on 10/6/24.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUserView()
+}
---
+++ SwiftDataProject/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Álvaro Gascón on 10/6/24.
+//
+
+import Foundation
